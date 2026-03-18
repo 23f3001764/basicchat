f7419ecbdfcffce7044e1517b259a86ebe12d589
23f3001764 <23f3001764@ds.study.iitm.ac.in>
2026-03-18 19:45:18 +0530

    initial commit

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAqFOl_Xmj7t_9bf99R2dcJOtFHwGxCgno",
-  authDomain: "basicchat-19d4a.firebaseapp.com",
-  databaseURL: "https://basicchat-19d4a-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "basicchat-19d4a",
-  storageBucket: "basicchat-19d4a.firebasestorage.app",
-  messagingSenderId: "999245871466",
-  appId: "1:999245871466:web:9f0937b742df8f35b4cd13"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
